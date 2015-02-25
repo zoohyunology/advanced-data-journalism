71700d2034db9fe56e32d9e7ad5ef1ef233b4dfd
Chase Davis <chase.davis@gmail.com>
2015-02-25 11:08:17 -0500

    Update clean-donors.py

--- spring-2015/campaign-finance-assignment/clean-donors.py
+++ spring-2015/campaign-finance-assignment/clean-donors.py
@@ -1,6 +1,6 @@
 # This variable represents the data from the nycdonors-cleanme.csv file as a
 # list, with each item representing a string for each row. 
-data = open('nycdonors-cleanme.csv', 'r').read().split('\r\n')[1:]
+data = open('nycdonors-cleanme.csv', 'r').readlines()[1:]
 
 # You'll add your output data here, using the .append() method
 output = []
