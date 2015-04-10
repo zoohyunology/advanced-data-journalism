aa645da2dc050e2ceb54a58778f22c062a96df17
Davis <195166@chase-davis-8730.nyhq.nytint.com>
2015-04-10 10:29:46 -0400

    Add JSON route

---
+++ spring-2015/flask-simple/app.py
@@ -0,0 +1,42 @@
+import json
+from flask import Flask, g, render_template, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/json")
+def sample_json():
+    data = {
+        'bills': [
+            {
+                'title': "Modifies death certification laws to include advanced practice registered nurses, assistant physicians, and physician assistants",
+                'created_at': "2015-02-26 01:37:57",
+                'updated_at': "2015-04-10 00:55:33",
+                'id': "MOB00007111",
+                'chamber': "upper",
+                'state': "mo",
+                'session': "2015",
+                'type': [
+                    "bill"
+                ],
+                'subjects': [
+                    "Transportation"
+                ],
+                'bill_id': "SB 517"
+            }
+        ]
+    }
+
+    # Turn the data into JSON
+    template = json.dumps(data)
+
+    # Return JSON to the browser
+    response = make_response(template)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
