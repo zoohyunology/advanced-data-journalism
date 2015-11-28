8a6e13137fa19c1f88cb13f09a407ad89ec86278
Chase Davis <cjdd3b@Chases-MacBook.local>
2015-11-28 14:54:31 -0500

    Web app materials

---
+++ web_app/app.py
@@ -0,0 +1,30 @@
+import json, csv
+from flask import Flask, g, render_template, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/template-test")
+def crime_static():
+    return render_template("template-test.html")
+
+@app.route("/json")
+def sample_json():
+
+    data = list(csv.DictReader(open("dispatches.csv", 'rU')))
+
+    # Turn the data into JSON
+    json_string = json.dumps(data)
+
+    # Return JSON to the browser
+    response = make_response(json_string)
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
