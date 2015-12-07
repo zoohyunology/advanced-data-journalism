ce5c7c9c9c71549f0600b17dd8b3f6744404ccea
Chase Davis <cjdd3b@Chases-MacBook.local>
2015-12-07 08:05:41 -0500

    Add client and server templates

--- web_app/app.py
+++ web_app/app.py
@@ -3,15 +3,33 @@ from flask import Flask, g, render_template, make_response
 
 app = Flask(__name__)
 
+########## TEST EXAMPLES ##########
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route("/template-test")
-def crime_static():
-    return render_template("template-test.html")
+@app.route('/template-test/<name>')
+def template_test(name=None):
+    capitalized_name = name.upper()
+    return render_template("template-test.html", test=capitalized_name)
 
-@app.route("/json")
+
+########## SERVER-SIDE RENDERING ##########
+
+@app.route("/incidents-server")
+def template_server():
+    data = list(csv.DictReader(open("dispatches.csv", 'rU')))
+    return render_template("template-server.html", data=data)
+
+########## CLIENT-SIDE RENDERING ##########
+
+@app.route("/incidents-client")
+def template_client():
+    return render_template("template-client.html")
+
+@app.route("/incidents.json")
 def sample_json():
 
     data = list(csv.DictReader(open("dispatches.csv", 'rU')))
