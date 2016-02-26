2df7e4fe4e815fecf881b0c9a5b40398e1814cce
Chase Davis <chase.davis@gmail.com>
2016-02-26 11:06:39 -0500

    Assignment

--- 2016-02-22-week6/cleaner.py
+++ 2016-02-22-week6/cleaner.py
@@ -1,12 +1,16 @@
 import csv
 
-# Open our input and output files
 csvfile = open('./data/sample.csv', 'r')
 outfile = open('./data/sample-clean.csv', 'w')
 
+#print csvfile.read()
+
 # Now a DictReader and DictWriter
+# DictReader and DictWriter are imported libraries
 reader = csv.DictReader(csvfile)
 writer = csv.DictWriter(outfile, reader.fieldnames)
+# DictWriter writes to outfile
+#reader.fieldname refers to the headers
 
 # Write headers
 writer.writeheader()
@@ -14,4 +18,6 @@ writer.writeheader()
 # Clean and write the data to output
 for row in reader:
     row['first_name'] = row['first_name'].upper()
+    #we are overwriting something that already exists with the clean version
+    print row
     writer.writerow(row)
