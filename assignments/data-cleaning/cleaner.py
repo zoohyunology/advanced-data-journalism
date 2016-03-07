0e548bcb63e571c4bedf7e47a15261846375467e
Lee Ju-hyun <zoohyunology@Zoohyunology.PK5001Z>
2016-03-07 08:55:00 -0600

    assignment

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,17 +1,9 @@
 import csv
 
-# define and open our input and output files
 old = open('./data/cleanme.csv', 'r')
-#new = open('./data/cleanedversion.csv', 'w')
 
-# now use DictReader and DictWriter methods within the files
 reader = csv.DictReader(old)
-#writer = csv.DictWriter(new, reader.fieldnames)
 
-# write headers
-#writer.writeheader()
-
-# clean
 for taco in reader: 
     taco['first_name'] = taco['first_name'].upper()
     taco['zip'] = taco['zip'].zfill(5)
