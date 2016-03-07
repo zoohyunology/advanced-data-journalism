0a72505790e077fc99002605cf8a59b6bc31e10e
Lee Ju-hyun <zoohyunology@mwc-022109.dhcp.missouri.edu>
2016-03-07 10:08:06 -0600

    assignment

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,12 +1,16 @@
 import csv
 
 old = open('./data/cleanme.csv', 'r')
+new = open('./data/cleanedversion.csv', 'w')
 
 reader = csv.DictReader(old)
+writer = csv.DictWriter(new, reader.fieldnames)
+
+writer.writeheader()
 
 for taco in reader: 
     taco['first_name'] = taco['first_name'].upper()
     taco['zip'] = taco['zip'].zfill(5)
     taco['city'] = taco['city'].replace('&nbsp;',' ')
     if int(taco['amount']) >= 1000:
-        print taco
+        writer.writerow(taco)
