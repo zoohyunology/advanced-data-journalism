fd5248b03db0e7632897f5fc95ba0a0b46dbe6fd
Lee Ju-hyun <zoohyunology@Zoohyunology.PK5001Z>
2016-03-07 08:48:09 -0600

    assignment

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,1 +1,20 @@
-# Your code goes here
+import csv
+
+# define and open our input and output files
+old = open('./data/cleanme.csv', 'r')
+new = open('./data/cleanedversion.csv', 'w')
+
+# now use DictReader and DictWriter methods within the files
+reader = csv.DictReader(old)
+writer = csv.DictWriter(new, reader.fieldnames)
+
+# write headers
+writer.writeheader()
+
+# clean
+for taco in reader: 
+    taco['first_name'] = taco['first_name'].upper()
+    taco['zip'] = taco['zip'].zfill(5)
+    taco['city'] = taco['city'].replace('&nbsp;',' ')
+    if int(taco['amount']) >= 1000:
+        print taco
